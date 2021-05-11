2e7c70b101b5590a0111b7dbb2d87b4967b8d959
Kishen35 <kishenshanthan@gmail.com>
2021-05-11 13:09:57 +0800

    added functionality to track packages using "track trackingID courierName

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 // DISCORD.JS
-
-// require the discord.js module
 const Discord = require('discord.js');
 
 // create a new Discord client
@@ -20,12 +18,15 @@ client.login(process.env.TOKEN);
 
 // Reply to messages || ${prefix} based on value in .env file
 client.on('message', message => {
-	// Pinging
+	if (!message.content.startsWith(`${process.env.prefix}`) || message.author.bot) return; // if message does not start with prefix or if author is bot, skip.
+
+	// Ping
     if (message.content.startsWith(`${process.env.prefix}ping`)) { // responds as long as startsWith given value
 		// send back "Pong." to the channel the message was sent in
 		message.channel.send('Pong.');
 	}
 
+    // Beep
     else if (message.content === `${process.env.prefix}beep`) {
 		message.channel.send(`boop ${message.author} from ${message.guild.name} in ${message.guild.region}`); //mentions user, server name and region
 	}
@@ -33,5 +34,35 @@ client.on('message', message => {
     // Fun
     else if (message.content === "chumete chumete") {
 		message.channel.send('tobikiri igai');
+	}
+
+    // Package Tracker
+    else if (message.content.startsWith(`${process.env.prefix}track`)) {
+        const args = message.content.slice(`${process.env.prefix}`.length).trim().split(' '); // args creates an array of the parameters passed after command
+        const command = args.shift().toLowerCase(); // removes the command (first element in args) from the array
+
+        const bodyParser = require('body-parser');
+
+        const packageTracker = require('./package-tracker/post');
+
+		if (!args.length){
+            message.channel.send("You didn't tell me your package number and courier \n Hint: '\"track {package_number} {courier}'");
+        }
+        else if (args.length == 1){
+            var postData = {"tracking_number": `${args[0]}`};
+            var url      = 'http://api.tracktry.com/v1/carriers/detect';
+        }
+        else {
+            var postData = null;
+            var url      = `http://api.tracktry.com/v1/trackings/${args[1]}/${args[0]}`;
+            const wait=ms=>new Promise(resolve => setTimeout(resolve, ms));
+            packageTracker.fetcher(url, "GET");
+            wait(4*1000).then(() => {
+                console.log(packageTracker.status);
+                message.channel.send(`**Status : ** ${packageTracker.status} \n${packageTracker.lastEvent}`);
+            }).catch(() => {
+                failureCallback();
+            });
+        }
 	}
 });
---
+++ package-tracker/post.js
@@ -0,0 +1,30 @@
+// Using Node-Fetch
+const fetch = require('node-fetch');
+const dotenv = require('dotenv');
+dotenv.config();
+
+exports.fetcher = (url, action) => {
+    fetch(url, {
+        method: action,
+        headers: {
+            'Content-Type': 'application/json',
+            'Tracktry-Api-Key': `${process.env.TracktryApiKey}`
+        }
+    })
+    .then(res => res.json())
+    .then(json => {
+        try{
+        console.log(json);
+        console.log(json.data[0].status);
+        console.log(json.data[0].lastEvent);
+        console.log(json.data[0].lastUpdateTime);
+
+        exports.status = json.data[0].status;
+        exports.lastEvent = json.data[0].lastEvent;
+        exports.lastUpdateTime = json.data[0].lastUpdateTime;
+        }
+        catch (error){
+            
+        }
+    });
+};
